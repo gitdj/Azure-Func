b033893b32dc4f48c2b5c2655b151b718a5b0125
DJ <biztalkdj@gmail.com>
2019-01-10 11:42:43 +0530

    asdasd
    asd

---
+++ BizTalkToAzureMigration.AzureFunction/AzureFunction.Helper/TransformHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace AzureFunction.Helper
+{
+    public class TransformHelper
+    {
+
+        public string Transform(string mapType, string req)
+        {
+            //return XslCompiledTransform();
+            string input = req;
+            Stream stylesheet = null;
+            Stream extension_object = null;
+
+            BizTalkMapType MapType = BizTalkMapType.InToOutMap;
+            
+            MapInitializer(ref input, ref stylesheet, ref extension_object, MapType);
+
+           return XslCompiledTransform(input, stylesheet, extension_object);
+        }
+
+        private static string XslCompiledTransform(string document, Stream stylesheet, Stream extensions)
+        {
+            return XslCompiledTransforms(document, stylesheet, ParseExtension(extensions));
+        }
+        private static string XslCompiledTransforms(string document, Stream stylesheet, object[] extension)
+        {
+            var resolver = new XmlUrlResolver();
+            XslCompiledTransform transform = new XslCompiledTransform(true);
+            transform.Load(XmlReader.Create(stylesheet), new XsltSettings(true, true), resolver);
+            XsltArgumentList arguments = new XsltArgumentList();
+            for (int index = 0; index < extension.Length; index += 2)
+            {
+                arguments.AddExtensionObject(
+                extension[index] as string,
+                extension[index + 1]);
+            }
+            StringBuilder output = new StringBuilder();
+            var stream = new MemoryStream();
+            var inputwriter = new StreamWriter(stream);
+            inputwriter.Write(document);
+            inputwriter.Flush();
+            stream.Position = 0;
+            
+            using (XmlWriter writer = XmlWriter.Create(output, transform.OutputSettings))
+                transform.Transform(XmlReader.Create(stream), arguments, writer);
+            return output.ToString();
+        }
+        private static object[] ParseExtension(Stream extension)
+        {
+            if ((extension.Length==0))
+                return new object[] { };
+            XmlDocument document = new XmlDocument();
+            document.Load(extension);
+            ArrayList extensions = new ArrayList();
+            foreach (XmlNode node in document.SelectNodes("/ExtensionObjects/ExtensionObject"))
+            {
+                string extension_namespace = node.Attributes["Namespace"].Value;
+                string extension_assembly = node.Attributes["AssemblyName"].Value;
+                string extension_class = node.Attributes["ClassName"].Value;
+                string assembly_qualified_name = String.Format("{0}, {1}"
+                , extension_class
+                , extension_assembly
+                ); object extension_object = Activator.CreateInstance(Type.GetType(assembly_qualified_name));
+                extensions.Add(extension_namespace);
+                extensions.Add(extension_object);
+            }
+
+            return extensions.ToArray();
+        }
+        private static void MapInitializer(ref string input, ref Stream stylesheet, ref Stream extension_object, BizTalkMapType MapType)
+        {
+            string basedirectorypath = "";
+            switch (MapType)
+            {
+                case BizTalkMapType.InToOutMap:
+                    //input =  @"\XSLT\IOFiles\MsgIn_output.xml";
+                    stylesheet = GetFromResources("XSLT.InToOut.xsl");
+                    extension_object = GetFromResources("XSLT.InToOut_extxml.xml");
+                    break;
+            }
+        }
+        private enum BizTalkMapType
+        {
+            InToOutMap = 2,
+            OneToOneMap = 1
+
+        }
+
+        internal static Stream GetFromResources(string resourceName)
+        {
+            Assembly assem = Assembly.GetExecutingAssembly();
+            return assem.GetManifestResourceStream(assem.GetName().Name + '.' + resourceName);
+        }
+    }
+}
+
---
+++ BizTalkToAzureMigration.AzureFunction/TransformationHelper/TransformHttpTrigger.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using AzureFunction.Helper;
+namespace TransformationHelper
+{
+    public static class TransformHttpTrigger
+    {
+        [FunctionName("TransformAPI")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            //if (name == null)
+            //{
+            //    // Get request body
+            //    dynamic data = await req.Content.ReadAsAsync<object>();
+            //    name = data?.name;
+            //}
+
+            string request = await req.Content.ReadAsStringAsync();
+
+            TransformHelper helperobj = new TransformHelper();
+            name = helperobj.Transform(name, request);
+          
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+        }
+    }
+}
